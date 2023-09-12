e8c818819cef5f80e22ebaf58b99c452653f7f78
phreis <philippr01@yahoo.de>
2023-09-12 08:51:49 +0200

    Imlement stretch TODOs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,95 @@
 import randomColor from 'randomcolor';
 import { useState } from 'react';
+import { keyframes } from 'styled-components';
+
+function ColoredBox(props) {
+  return (
+    <div
+      style={{
+        backgroundColor: props.randomColorCode,
+        width: `${props.size * 20}px`,
+        height: `${props.size * 10}px`,
+        transition: 'background-color 1000ms linear',
+      }}
+    >
+      Generated Color: {props.randomColorCode}
+    </div>
+  );
+}
+
+function ControlPanel(props) {
+  const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+  return (
+    <div
+      style={{
+        border: 'solid 2px',
+        padding: '10px',
+        width: '200px',
+        height: 'auto',
+        /*         animation: `${spin} 2s linear infinite`, */
+      }}
+    >
+      <label htmlFor="color-picker-hue">Hue </label>
+      <input
+        onInput={(event) => {
+          const selectedColor = event.target.value;
+          props.setRandomColorCode(
+            props.getNewRandomColorCode(selectedColor, props.lightness),
+          );
+          props.setColor(selectedColor);
+        }}
+        value={props.color}
+        type="color"
+        id="color-picker-hue"
+      />
+      <br />
+      <label htmlFor="lighness">Lighness </label>
+      <select
+        name="lighness"
+        id="lighness"
+        onInput={(event) => {
+          const selectedLightness = event.target.value;
+          props.setRandomColorCode(
+            props.getNewRandomColorCode(props.color, selectedLightness),
+          );
+          props.setLightness(selectedLightness);
+        }}
+      >
+        <option value="bright">Bright</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+      <br />
+      <div>
+        <label htmlFor="Size">Size </label>
+        <input
+          type="range"
+          id="size"
+          name="size"
+          min="1"
+          max="20"
+          onInput={(event) => props.setSize(event.target.value)}
+          value={props.size}
+        />
+      </div>
+      <br />
+      <br />
+      <button
+        onClick={() =>
+          props.setRandomColorCode(
+            props.getNewRandomColorCode(props.color, props.lightness),
+          )
+        }
+        id="random-generate-button"
+      >
+        Generate
+      </button>
+    </div>
+  );
+}
 
 export default function App() {
   const [color, setColor] = useState(
@@ -9,59 +99,33 @@ export default function App() {
   const [lightness, setLightness] = useState('light');
 
   const [randomColorCode, setRandomColorCode] = useState(
-    getNewRandomColorCode(),
+    getNewRandomColorCode(color, lightness),
   );
 
-  function getNewRandomColorCode() {
+  const [size, setSize] = useState(10);
+
+  function getNewRandomColorCode(hue, luminosity) {
     return randomColor({
-      luminosity: lightness,
-      hue: color,
+      luminosity: luminosity,
+      hue: hue,
     });
-
-    // '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0')
   }
-
   return (
     <div>
       <h1>React Random Color Generator</h1>
-      <div style={{ backgroundColor: randomColorCode }}>
-        Generated Color: {randomColorCode}
-      </div>
       <br />
-      <label htmlFor="random-generate-button">Random Color: </label>
-      <button
-        onClick={() => setRandomColorCode(getNewRandomColorCode())}
-        id="random-generate-button"
-      >
-        Generate
-      </button>
-      <br />
-      <br />
-      <label htmlFor="color-picker-hue">Change hue: </label>
-      <input
-        onInput={(event) => {
-          const selectedColor = event.target.value;
-          setRandomColorCode(selectedColor);
-          setColor(selectedColor);
-        }}
-        value={randomColorCode}
-        type="color"
-        id="color-picker-hue"
+      <ControlPanel
+        setRandomColorCode={setRandomColorCode}
+        getNewRandomColorCode={getNewRandomColorCode}
+        color={color}
+        setColor={setColor}
+        lightness={lightness}
+        setLightness={setLightness}
+        size={size}
+        setSize={setSize}
       />
       <br />
-      <label htmlFor="lighness">Choose Lighness:</label>
-      <select
-        name="lighness"
-        id="lighness"
-        onInput={(event) => {
-          setLightness(event.target.value);
-          setRandomColorCode(getNewRandomColorCode());
-        }}
-      >
-        <option value="bright">Bright</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-      </select>
+      <ColoredBox randomColorCode={randomColorCode} size={size} />
     </div>
   );
 }
